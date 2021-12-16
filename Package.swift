7f26d2769c11963fd85577bb770b6b207a3c65b8
Sergejs Smirnovs <sergey@nbi.me>
2021-12-16 13:04:13 +0100

    Removed Dynamic

--- Package.swift
+++ Package.swift
@@ -14,7 +14,6 @@ let package = Package(
     products: [
         .library(
             name: "ServiceContainer",
-            type: .dynamic,
             targets: ["ServiceContainer"]
         ),
     ],
